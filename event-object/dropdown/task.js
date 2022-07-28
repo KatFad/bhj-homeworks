a8a0e97f223ab1689e9e45a0772a52ada951c676
KatFad <q46q46@mail.ru>
2022-07-28 15:29:28 +0300

    'dz_1.1'

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+const dropdownValue = Array.from(document.querySelectorAll('.dropdown__value')),
+    dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'))
+
+for (let item of dropdownValue) {
+   item.onclick = dropdown
+}
+
+function dropdown() {
+    this.closest('.dropdown').querySelector('.dropdown__list').classList.toggle('dropdown__list_active');
+    return false
+}
+
+for (let item of dropdownItem) {
+    item.onclick = dropItem
+}
+
+function dropItem() {
+    this.closest('.dropdown__list').closest('.dropdown').querySelector('.dropdown__value').textContent = this.textContent;
+    return false
+}
