81f763bf85f3558ddfc5a110dbd01bd1df29f4e1
KatFad <q46q46@mail.ru>
2022-07-29 09:11:01 +0300

    'dz_1.2+_corrected'

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,7 +1,9 @@
+/*
 document.querySelector('body').innerHTML = document.querySelector('body').innerHTML
     +'<ul class="menu menu_main" style="margin-top: 100px">'
     + document.querySelector('.menu').innerHTML + '</ul>';
 
+*/
 const menuArray = Array.from(document.querySelectorAll('.menu__link'))
 
 for(let item of menuArray) {
@@ -9,12 +11,23 @@ for(let item of menuArray) {
         ,menuActive =item.closest('.menu_main').querySelectorAll('.menu_sub')
 
     item.onclick = () => {
-        if (menuParent) {
-            menuActive.forEach((element) => element.classList.remove('menu_active'))
-            !menuParent.classList.contains('menu_active') ? menuParent.classList.add('menu_active') : menuSub.classList.remove('menu_active')
-        }
-        return false
 
-    }
+        if (menuParent) {
+
+            !menuParent.classList.contains('menu_active') ? (menuActive.forEach((element) => element.classList.remove('menu_active')),menuParent.classList.toggle('menu_active')) : menuParent.classList.remove('menu_active')
+           /* if(!menuParent.classList.contains('menu_active')) {
+                menuActive.forEach((element) => element.classList.remove('menu_active'))
+                menuParent.classList.toggle('menu_active')
+            }
+            else {
+                menuParent.classList.remove('menu_active')
+            }*/
+            return false
+        }
+
+
 }
+
+}
+
 
