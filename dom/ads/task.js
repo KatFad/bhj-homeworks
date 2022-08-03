190f96e79eff7693b52521aee85b1dc0af0c5cd1
KatFad <q46q46@mail.ru>
2022-08-03 12:59:16 +0300

    'dz_1.2'

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,21 +1,24 @@
+/*
+const content = document.querySelector('.content');
+const card = document.querySelector('.card');
+content.innerHTML = content.innerHTML + '<div class="card">' + card.innerHTML + '</div>';
+content.innerHTML = content.innerHTML + '<div class="card">' + card.innerHTML + '</div>';
+content.innerHTML = content.innerHTML + '<div class="card">' + card.innerHTML + '</div>';
+*/
+
 const rotatorCases = Array.from(document.querySelectorAll('.rotator__case'));
-let speed = 1000;
+let speed = 1000, num=0;
 
-function ads() {
-    const nextRotatorCase = document.querySelector('.rotator__case_active').nextElementSibling;
-    let activeCaseIndex = rotatorCases.findIndex((item) => item.classList.contains('rotator__case_active'));
 
-    rotatorCases.forEach((element) => {
-        element.classList.remove('rotator__case_active')
-        element.style.color = element.dataset.color;
-        speed = element.dataset.speed
-    });
-
-    (activeCaseIndex === rotatorCases.length - 1) ? rotatorCases[0].classList.add('rotator__case_active') : nextRotatorCase.classList.add('rotator__case_active');
-
+function ads(num) {
+    rotatorCases[num].style.color = rotatorCases[num].dataset.color;
+    speed = rotatorCases[num].dataset.speed
 }
 
-setInterval(ads, speed);
-
-
+setInterval(()=>{
+    rotatorCases[num].classList.remove('rotator__case_active')
+    num = (num >= rotatorCases.length - 1) ? 0 : ++num;
+    rotatorCases[num].classList.toggle('rotator__case_active')
+    ads(num)
+}, speed);
 
